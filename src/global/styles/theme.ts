9ec6347c9a6aef54cd2d625460b135f629180644
cardosorenanalves <renancardoso0307@gmail.com>
2022-07-14 15:00:05 -0300

    Paleta de cores criado

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default{
+    colors: {
+        primary: '#5636D3',
+
+        secondary: '#FF872C',
+        secondary_ligth: 'rgba(255, 135, 44, 0.3)',
+
+        success: '#12A454',
+        success_ligth: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#E83F5B',
+        attention_ligth: 'rgba(232, 63, 91, 0.5)',
+
+        shape: '#FFFFF',
+        title: '#363F5F',
+        text: '#969CB2',
+        background: '#F0F2F5',
+    }
+}
